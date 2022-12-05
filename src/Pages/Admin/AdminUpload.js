245ecf052c5d9280e08fbddf687b61f154e4a5c1
Ayon Jodder <ayonjodder177@gmail.com>
2022-12-06 02:11:46 +0600

    Admin add product

--- src/Pages/Admin/AdminUpload.js
+++ src/Pages/Admin/AdminUpload.js
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import { ImCross } from 'react-icons/im';
 import { dataContext } from '../../App';
+import { BASE_URL } from '../../Utils/Urls';
 
 const AdminUpload = ({ openPopup }) => {
     const { loggedInUser, setOpenPopup } = useContext(dataContext);
@@ -30,7 +31,7 @@ const AdminUpload = ({ openPopup }) => {
                         role: loggedInUser?.result?.user?.role
                     }
                     //send data to db
-                    fetch(`https://floating-ocean-13139.herokuapp.com/blogs`, {
+                    fetch(`${BASE_URL}/api/v1/product/add`, {
                         method: 'POST',
                         headers: {
                             'content-type': 'application/json',
@@ -39,14 +40,13 @@ const AdminUpload = ({ openPopup }) => {
                     })
                         .then(res => res.json())
                         .then(inserted => {
-
-                            if (inserted.insertedId) {
-                                toast.success(`Your post ${data.title.slice(0, 5)}... added successfully.`);
-
+                            if (inserted.success) {
+                                toast.success(`Your post ${data.label.slice(0, 5)}... added successfully.`);
                                 reset()
+                                setOpenPopup(false)
                             }
                             else {
-                                toast.error('Failed to add a Article')
+                                toast.error('Failed to add a Product')
                             }
                         }
                         )
@@ -58,7 +58,24 @@ const AdminUpload = ({ openPopup }) => {
             <div className="popup_content relative">
                 <ImCross onClick={() => setOpenPopup(false)} className='absolute right-0 top-0 mr-4 mt-4 h-4 w-4 cursor-pointer' />
                 <div>
-                    jhgjhgjgh
+                    <form onSubmit={handleSubmit(handleAddData)}>
+                        <div className="form-group">
+                            <label className='text-md font-medium' htmlFor="label">Title</label>
+                            <input type="text" className="form-control focus:border-[#37BC96] focus:outline-none w-full border border-slate-300 p-2 rounded-md" id="label" placeholder="Enter Title" {...register("label", { required: true })} />
+                            {errors.label && <span className='text-danger'>This field is required</span>}
+                        </div>
+                        <div className="form-group mt-3">
+                            <label className='text-md font-medium' htmlFor="details">Details</label>
+                            <textarea className="form-control focus:border-[#37BC96] focus:outline-none w-full border border-slate-300 p-2 rounded-md" id="details" placeholder='Details' rows="2" {...register("details", { required: true })}></textarea>
+                            {errors.details && <span className='text-danger'>This field is required</span>}
+                        </div>
+                        <div className="form-group mt-3">
+                            <input type="file" className="form-control-file" id="image" {...register("image", { required: true })} />
+                            {errors.image && <span className='text-danger'>This field is required</span>}
+                        </div>
+                        <button type="submit" className="bg-[#37BC96] text-white font-semibold mt-5 px-7 rounded-md py-2">Post</button>
+
+                    </form>
                 </div>
             </div>
         </div>
